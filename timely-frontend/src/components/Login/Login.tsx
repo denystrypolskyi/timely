f0c74bb11c359b7d42731f31bdc5d176889729b6
denystrypolskyi <denystrypolskyi@gmail.com>
2026-04-19 15:52:23 +0200

    add unit tests

--- timely-frontend/src/components/Login/Login.tsx
+++ timely-frontend/src/components/Login/Login.tsx
@@ -1,100 +1,99 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { LoginFormData } from "../../types/auth.types";
-import { useMe } from "../../hooks/useMe";
-import { useNavigate } from "react-router-dom";
+import {useState} from "react";
+import {useForm} from "react-hook-form";
+import {LoginFormData} from "../../types/auth.types";
+import {useMe} from "../../hooks/useMe";
+import {useNavigate} from "react-router-dom";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const Login = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginFormData>();
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const { login, isLoggingIn } = useMe();
-  const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm<LoginFormData>();
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const {login, isLoggingIn} = useMe();
+    const navigate = useNavigate();
 
-  const onSubmit = async (data: LoginFormData) => {
-    try {
-      await login(data);
-    } catch (error: any) {
-      const message = error?.response?.data?.message || "Login failed";
-      setErrorMessage(message);
+    const onSubmit = async (data: LoginFormData) => {
+        try {
+            await login(data);
+        } catch (error: any) {
+            const message = error?.response?.data?.message || "Login failed";
+            setErrorMessage(message);
+        }
+    };
+
+    const handleGoogleLogin = () => {
+        window.location.href =
+            import.meta.env.VITE_GOOGLE_OAUTH_REDIRECT_URL;
+    };
+
+    if (isLoggingIn) {
+        return <LoadingSpinner/>;
     }
-  };
 
-  const handleGoogleLogin = () => {
-    window.location.href =
-      "https://work-tracker-44f7.onrender.com/login/oauth2/code/google";
-    // window.location.href = "http://localhost:8080/oauth2/authorization/google";
-  };
+    return (
+        <div className="container">
+            <div className="formWrapper">
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-group">
+                        <input
+                            placeholder="Username"
+                            id="username"
+                            type="text"
+                            className="input"
+                            {...register("username", {required: "Username is required"})}
+                        />
+                        {errors.username && (
+                            <p className="error">{errors.username.message}</p>
+                        )}
+                    </div>
+                    <div className="form-group">
+                        <input
+                            placeholder="Password"
+                            id="password"
+                            type="password"
+                            className="input"
+                            {...register("password", {required: "Password is required"})}
+                        />
+                        {errors.password && (
+                            <p className="error">{errors.password.message}</p>
+                        )}
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "end",
+                            marginBottom: "16px",
+                        }}
+                    >
+                        <button className="button" type="submit" disabled={isLoggingIn}>
+                            Login
+                        </button>
 
-  if (isLoggingIn) {
-    return <LoadingSpinner />;
-  }
-
-  return (
-    <div className="container">
-      <div className="formWrapper">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-group">
-            <input
-              placeholder="Username"
-              id="username"
-              type="text"
-              className="input"
-              {...register("username", { required: "Username is required" })}
-            />
-            {errors.username && (
-              <p className="error">{errors.username.message}</p>
-            )}
-          </div>
-          <div className="form-group">
-            <input
-              placeholder="Password"
-              id="password"
-              type="password"
-              className="input"
-              {...register("password", { required: "Password is required" })}
-            />
-            {errors.password && (
-              <p className="error">{errors.password.message}</p>
-            )}
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "end",
-              marginBottom: "16px",
-            }}
-          >
-            <button className="button" type="submit" disabled={isLoggingIn}>
-              Login
-            </button>
-
-            <a onClick={() => navigate("/register")}>Don't have an account?</a>
-          </div>
-          <div className="oauth-container">
-            <div className="oauth-divider">
-              <div className="oauth-line left" />
-              <p>or login with</p>
-              <div className="oauth-line right" />
+                        <a onClick={() => navigate("/register")}>Don't have an account?</a>
+                    </div>
+                    <div className="oauth-container">
+                        <div className="oauth-divider">
+                            <div className="oauth-line left"/>
+                            <p>or login with</p>
+                            <div className="oauth-line right"/>
+                        </div>
+                        <img
+                            className="oauth-logo"
+                            src="/google.svg"
+                            alt="Login with Google"
+                            onClick={handleGoogleLogin}
+                        />
+                    </div>
+                </form>
+                {errorMessage && <p style={{color: "red"}}>{errorMessage}</p>}
             </div>
-            <img
-              className="oauth-logo"
-              src="/google.svg"
-              alt="Login with Google"
-              onClick={handleGoogleLogin}
-            />
-          </div>
-        </form>
-        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Login;
--- timely-frontend/src/components/Register/Register.tsx
+++ timely-frontend/src/components/Register/Register.tsx
@@ -1,119 +1,119 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { RegisterFormData } from "../../types/auth.types";
+import {useState} from "react";
+import {useForm} from "react-hook-form";
+import {RegisterFormData} from "../../types/auth.types";
 import authService from "../../services/auth.service";
-import { useNavigate } from "react-router-dom";
-import { useMe } from "../../hooks/useMe";
+import {useNavigate} from "react-router-dom";
+import {useMe} from "../../hooks/useMe";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const Register = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<RegisterFormData>();
-  const { login, isLoggingIn } = useMe();
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm<RegisterFormData>();
+    const {login, isLoggingIn} = useMe();
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const navigate = useNavigate();
 
-  const onSubmit = async (data: RegisterFormData) => {
-    try {
-      await authService.register(data);
-      const loginData = { username: data.username, password: data.password };
-      await login(loginData);
-      navigate("/me");
-    } catch (error: any) {
-      const message = error?.response?.data?.message || "Registration failed";
-      setErrorMessage(message);
+    const onSubmit = async (data: RegisterFormData) => {
+        try {
+            await authService.register(data);
+            const loginData = {username: data.username, password: data.password};
+            await login(loginData);
+            navigate("/me");
+        } catch (error: any) {
+            const message = error?.response?.data?.message || "Registration failed";
+            setErrorMessage(message);
+        }
+    };
+
+    const handleGoogleLogin = () => {
+        window.location.href = import.meta.env.VITE_GOOGLE_OAUTH_REDIRECT_URL;
+    };
+
+    if (isLoggingIn) {
+        return <LoadingSpinner/>;
     }
-  };
 
-  const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:8080/oauth2/authorization/google";
-  };
+    return (
+        <div className="container">
+            <div className="formWrapper">
+                <h2>Register</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-group">
+                        <input
+                            placeholder="Username"
+                            id="username"
+                            type="text"
+                            className="input"
+                            {...register("username", {required: "Username is required"})}
+                        />
+                        {errors.username && (
+                            <p className="error">{errors.username.message}</p>
+                        )}
+                    </div>
 
-  if (isLoggingIn) {
-    return <LoadingSpinner />;
-  }
+                    <div className="form-group">
+                        <input
+                            placeholder="Password"
+                            id="password"
+                            type="password"
+                            className="input"
+                            {...register("password", {required: "Password is required"})}
+                        />
+                        {errors.password && (
+                            <p className="error">{errors.password.message}</p>
+                        )}
+                    </div>
 
-  return (
-    <div className="container">
-      <div className="formWrapper">
-        <h2>Register</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-group">
-            <input
-              placeholder="Username"
-              id="username"
-              type="text"
-              className="input"
-              {...register("username", { required: "Username is required" })}
-            />
-            {errors.username && (
-              <p className="error">{errors.username.message}</p>
-            )}
-          </div>
+                    <div className="form-group">
+                        <input
+                            placeholder="Confirm password"
+                            id="confirmPassword"
+                            type="password"
+                            className="input"
+                            {...register("confirmPassword", {
+                                required: "Confirm Password is required",
+                            })}
+                        />
+                        {errors.confirmPassword && (
+                            <p className="error">{errors.confirmPassword.message}</p>
+                        )}
+                    </div>
 
-          <div className="form-group">
-            <input
-              placeholder="Password"
-              id="password"
-              type="password"
-              className="input"
-              {...register("password", { required: "Password is required" })}
-            />
-            {errors.password && (
-              <p className="error">{errors.password.message}</p>
-            )}
-          </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "end",
+                            marginBottom: "16px",
+                        }}
+                    >
+                        <button className="button" type="submit">
+                            Register
+                        </button>
+                        <a onClick={() => navigate("/login")}>Already have an account?</a>
+                    </div>
+                    <div className="oauth-container">
+                        <div className="oauth-divider">
+                            <div className="oauth-line left"/>
+                            <p>or register with</p>
+                            <div className="oauth-line right"/>
+                        </div>
+                        <img
+                            className="oauth-logo"
+                            src="/google.svg"
+                            alt="Login with Google"
+                            onClick={handleGoogleLogin}
+                        />
+                    </div>
+                </form>
 
-          <div className="form-group">
-            <input
-              placeholder="Confirm password"
-              id="confirmPassword"
-              type="password"
-              className="input"
-              {...register("confirmPassword", {
-                required: "Confirm Password is required",
-              })}
-            />
-            {errors.confirmPassword && (
-              <p className="error">{errors.confirmPassword.message}</p>
-            )}
-          </div>
-
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "end",
-              marginBottom: "16px",
-            }}
-          >
-            <button className="button" type="submit">
-              Register
-            </button>
-            <a onClick={() => navigate("/login")}>Already have an account?</a>
-          </div>
-          <div className="oauth-container">
-            <div className="oauth-divider">
-              <div className="oauth-line left" />
-              <p>or register with</p>
-              <div className="oauth-line right" />
+                {errorMessage && <p style={{color: "red"}}>{errorMessage}</p>}
             </div>
-            <img
-              className="oauth-logo"
-              src="/google.svg"
-              alt="Login with Google"
-              onClick={handleGoogleLogin}
-            />
-          </div>
-        </form>
-
-        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Register;
